d343f88ea5828271504f6ff295489db18f008836
Kwame Wright <kwamelaw@usc.edu>
2017-12-03 16:45:25 -0800

    Frame rate adaptation

--- car/vesper_controller.py
+++ car/vesper_controller.py
@@ -3,8 +3,9 @@ sys.path.append('../')
 
 import config as cfg
 
-from common import Controller
 from common import AvgItem
+from common import Controller
+from common import bounded
 from dispatcher import Dispatcher
 
 
@@ -13,6 +14,11 @@ class VesperController(Controller):
     EWMA_ALPHA   = 0.8
 
     VAL_AVG_FPS = 'FPS.avg'
+    VAL_FRAME_RATE = 'Frame Rate'
+    VAL_T_0 = 'T_o'
+    VAL_M_0 = 'M_o'
+
+    DRONE_NAME = 'drone'
 
     def __init__(self):
         Controller.__init__(self)
@@ -22,9 +28,11 @@ class VesperController(Controller):
         self.metrics = {}
 
         self.values = {}
-        self.values['T_o'] = cfg.T_o
-        self.values['M_o'] = cfg.M_o
+        self.values[self.VAL_T_0] = cfg.T_o
+        self.values[self.VAL_M_0] = cfg.M_o
         self.values[self.VAL_AVG_FPS] = 0.0
+
+        self.connected = set()
 
         return
 
@@ -36,19 +44,32 @@ class VesperController(Controller):
     def loop(self):
         """Controller action loop."""
         self.log().info('controller loop')
+
+        if self.DRONE_NAME in self.connected:
+            # Adjust frame rate
+            avg_fps = self.values[self.VAL_AVG_FPS]
+            t0 = self.values[self.VAL_T_0]
+            ratio = float(t0)/avg_fps
+
+            rate = bounded(t0*ratio, 0.8 * t0, 1.2 * t0)
+            self.set_frame_rate(t0 * ratio)
+
         return
 
     def logon(self, name):
         """Handles device logon."""
         Controller.logon(self, name)
+        self.connected.add(name)
 
-        if name == 'drone':
-            params = [
-                ('frame_rate', 1.0),
-                ('camera', True)
-            ]
-            self.send_params(name, params)
+        if name == self.DRONE_NAME:
+            self.set_frame_rate(self.values[self.VAL_T_0])
 
+        return
+
+    def logoff(self, name):
+        """Handles device logoff."""
+        Controller.logoff(self, name)
+        self.connected.discard(name)
         return
 
     def put_metrics(self, metrics):
@@ -60,8 +81,20 @@ class VesperController(Controller):
             self.avg_fps.add(fps)
             self.values[self.VAL_AVG_FPS] = self.avg_fps.pull()
 
+        self.log().debug('values: %s', self.values)
         return
 
     def get_values(self):
         """Retrieves controller's data."""
         return self.values
+
+    def set_frame_rate(self, rate):
+        """Sets frame rate."""
+        params = [
+            ('frame_rate', rate),
+            ('camera', True)
+        ]
+        self.send_params(self.DRONE_NAME, params)
+
+        self.values[self.VAL_FRAME_RATE] = rate
+        return
