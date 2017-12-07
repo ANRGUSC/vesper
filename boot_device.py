853d7be48c0ccdbb93b4db884fc852dd4acb8ba0
Kwame Wright <kwamelaw@usc.edu>
2017-12-06 18:13:16 -0800

    Added name argument to boot_device script.

--- boot_device.py
+++ boot_device.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import argparse
 import copy
 import logging.config
 
@@ -11,12 +12,17 @@ if __name__ == '__main__':
     # Logging setup
     mylogcfg = copy.deepcopy(cfg.LOGCFG)
     mylogcfg['handlers']['default']['level'] = 'DEBUG'
-    mylogcfg['handlers']['file']['filename'] = 'drone_output.log'
+    mylogcfg['handlers']['file']['filename'] = 'device_output.log'
     logging.config.dictConfig(mylogcfg)
 
     log = logging.getLogger('boot_device')
 
-    name = 'device'
+    # Command-line arguments
+    parser = argparse.ArgumentParser(description='Device boot script')
+    parser.add_argument('name', type=str, help='device name')
+    args = parser.parse_args()
+
+    name = args.name
     device = Device(name)
 
     log.info('booting device %s...', name)
