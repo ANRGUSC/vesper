b0b284530b4aa0c31c884c859b20fcedc8dab8d2
Kwame Wright <kwamelaw@usc.edu>
2017-12-07 18:26:33 -0800

    Command line option for log level.

--- boot_device.py
+++ boot_device.py
@@ -9,18 +9,20 @@ import config as cfg
 from device import Device
 
 if __name__ == '__main__':
+    # Command-line arguments
+    parser = argparse.ArgumentParser(description='Device boot script')
+    parser.add_argument('name', type=str, help='device name')
+    parser.add_argument('-l', '--loglevel', default='INFO',
+                        help='log level (DEBUG|INFO|WARNING|ERROR|CRITICAL)')
+    args = parser.parse_args()
+
     # Logging setup
     mylogcfg = copy.deepcopy(cfg.LOGCFG)
-    mylogcfg['handlers']['default']['level'] = 'DEBUG'
+    mylogcfg['handlers']['default']['level'] = args.loglevel.upper()
     mylogcfg['handlers']['file']['filename'] = 'device_output.log'
     logging.config.dictConfig(mylogcfg)
 
     log = logging.getLogger('boot_device')
-
-    # Command-line arguments
-    parser = argparse.ArgumentParser(description='Device boot script')
-    parser.add_argument('name', type=str, help='device name')
-    args = parser.parse_args()
 
     name = args.name
     device = Device(name)
