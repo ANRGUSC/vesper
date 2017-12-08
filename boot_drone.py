741d20304fb205841490d1b40b6854668efe756b
Kwame Wright <kwamelaw@usc.edu>
2017-12-07 18:30:21 -0800

    Updated drone boot script.

--- boot_drone.py
+++ boot_drone.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import argparse
 import copy
 import logging.config
 
@@ -9,9 +10,15 @@ from drone import Camera
 from drone import Drone
 
 if __name__ == '__main__':
+    # Command-line arguments
+    parser = argparse.ArgumentParser(description='Drone boot script')
+    parser.add_argument('-l', '--loglevel', default='INFO',
+                        help='log level (DEBUG|INFO|WARNING|ERROR|CRITICAL)')
+    args = parser.parse_args()
+
     # Logging setup
     mylogcfg = copy.deepcopy(cfg.LOGCFG)
-    mylogcfg['handlers']['default']['level'] = 'INFO'
+    mylogcfg['handlers']['default']['level'] = args.loglevel.upper()
     mylogcfg['handlers']['file']['filename'] = 'drone_output.log'
     logging.config.dictConfig(mylogcfg)
 
